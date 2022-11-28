6ed00ffae7e6a20f19b6badc8db4c4d0d0019f56
rafleberry <kraiglyons0929@gmail.com>
2022-11-28 23:48:16 +0800

    fix: homepage fixed

--- features/home/components/SelectedNFT.tsx
+++ features/home/components/SelectedNFT.tsx
@@ -80,7 +80,10 @@ const SelectedNFT = () => {
           res_collection.logo +
           PINATA_SECONDARY_IMAGE_SIZE,
         collection_name: res_collection.name,
-        nft_uri: res_nft.uri + PINATA_PRIMARY_IMAGE_SIZE,
+        nft_uri:
+          process.env.NEXT_PUBLIC_PINATA_URL +
+          res_nft.uri +
+          PINATA_PRIMARY_IMAGE_SIZE,
         price: res_nft.price,
         paymentToken: res_nft.paymentToken,
       };
