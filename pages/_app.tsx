bef27359db72d0c4c4bf36546267f48af4bd7d16
Florin <mineloom@gmail.com>
2023-04-17 21:02:56 +0300

    fbpixel setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,6 +28,10 @@ import { config } from "services/config";
 import React from "react";
 import { wrapper, store } from "../store/store";
 import { Provider } from "react-redux";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as fbq from "../lib/fpixel";
+import { useEffect } from "react";
 
 function SafeHydrate({ children }) {
   return (
@@ -38,38 +42,73 @@ function SafeHydrate({ children }) {
 }
 
 function MyApp({ Component, pageProps }: any) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview();
+
+    const handleRouteChange = () => {
+      fbq.pageview();
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <SafeHydrate>
-          <ChakraProvider>
-            <SdkProvider config={config}>
-              {/* <ErrorBoundary> */}
-              <Provider store={store}>
-                <Component {...pageProps} />
-                {__TEST_MODE__ && <TestnetDialog />}
-                <Portal>
-                  <ToastContainer
-                    position="top-right"
-                    autoClose={5000}
-                    hideProgressBar={true}
-                    newestOnTop
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    toastStyle={{ zIndex: 150 }}
-                    style={{ width: "auto" }}
-                  />
-                </Portal>
-              </Provider>
-              {/* </ErrorBoundary> */}
-            </SdkProvider>
-          </ChakraProvider>
-        </SafeHydrate>
-      </QueryClientProvider>
-    </RecoilRoot>
+    <>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', ${fbq.FB_PIXEL_ID});
+        `,
+        }}
+      />
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <SafeHydrate>
+            <ChakraProvider>
+              <SdkProvider config={config}>
+                {/* <ErrorBoundary> */}
+                <Provider store={store}>
+                  <Component {...pageProps} />
+                  {__TEST_MODE__ && <TestnetDialog />}
+                  <Portal>
+                    <ToastContainer
+                      position="top-right"
+                      autoClose={5000}
+                      hideProgressBar={true}
+                      newestOnTop
+                      closeOnClick
+                      rtl={false}
+                      pauseOnFocusLoss
+                      draggable
+                      pauseOnHover
+                      toastStyle={{ zIndex: 150 }}
+                      style={{ width: "auto" }}
+                    />
+                  </Portal>
+                </Provider>
+                {/* </ErrorBoundary> */}
+              </SdkProvider>
+            </ChakraProvider>
+          </SafeHydrate>
+        </QueryClientProvider>
+      </RecoilRoot>
+    </>
   );
 }
 
